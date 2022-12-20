ee08aff3326650efe259b77ce1c952c0ad64f34e
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2022-12-20 15:08:20 +0900

    formated

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import * as core from '@actions/core';
+import { debug, getInput, setFailed, setOutput } from '@actions/core';
 import { wait } from '@/wait';
 
 async function run(): Promise<void> {
   try {
-    const path: string = core.getInput('path');
-    core.debug(`path: ${path}`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const path: string = getInput('path');
+    debug(`path: ${path}`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(path, 10));
-    core.debug(new Date().toTimeString());
+    debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    const ms = 1000;
+    await wait({ milliseconds: ms });
+    debug(new Date().toTimeString());
+
+    setOutput('time', new Date().toTimeString());
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) setFailed(error.message);
   }
 }
 

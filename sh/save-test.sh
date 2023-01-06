2465e6f0cf52982a607f42d4de6db944d143a4ef
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-01-06 15:17:02 +0900

    shell書き直した

---
+++ sh/save-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RUNNER_OS=LINUX
+FILE_HASH=ABRACADABRA
+TARGET_LANG=nodejs
+SAVE_PATH="./node_modules\n./package-lock.json"
+
+npm run dev --\
+  --path="${SAVE_PATH}" \
+  --key="${RUNNER_OS}-${TARGET_LANG}-${FILE_HASH}" \
+  --restore-keys="${RUNNER_OS}-${TARGET_LANG}-\n${RUNNER_OS}-" \
+  --aws-s3-bucket="${AWS_S3_BUCKET_NAME}" \
+  --aws-access-key-id="${AWS_ACCESS_KEY_ID}" \
+  --aws-secret-access-key="${AWS_SECRET_ACCESS_KEY}" \
+  --aws-region="${AWS_REGION}" \
+  --aws-endpoint="${AWS_ENDPOINT}" \
+  --aws-s3-bucket-endpoint=false \
+  --aws-s3-force-path-style=true

02e97a407c46c596b20cc854647cdfd348bd4c64
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-01-19 15:21:23 +0900

    テスト修正

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,20 +1,28 @@
 import * as cp from 'child_process';
 import * as path from 'path';
 import * as process from 'process';
-import { describe, test } from '@jest/globals';
+import { describe, test, beforeEach, afterEach } from '@jest/globals';
+
+beforeEach(() => {
+  process.env['GITHUB_EVENT_NAME'] = 'push';
+  process.env['GITHUB_REF'] = 'refs/heads/feature-branch';
+});
+
+afterEach(() => {
+  delete process.env['GITHUB_EVENT_NAME'];
+  delete process.env['GITHUB_REF'];
+});
 
 // shows how the runner will run a javascript action with env / stdout protocol
 describe('test runs', () => {
   const np = process.execPath;
   const options: cp.ExecFileSyncOptions = { env: process.env };
-  const argv = [
+  const argv: string[] = [
     '--path=node_modules',
     '--key=XXXX',
     '--aws-s3-bucket=XXXX',
     '--aws-access-key-id=XXXX',
     '--aws-secret-access-key=XXXX',
-    '--aws-s3-bucket-endpoint=true',
-    '--aws-s3-force-path-style=true',
   ];
 
   // TODO: 迂回処理が必要
